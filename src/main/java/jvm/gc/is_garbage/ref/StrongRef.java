f4101ee92347ae3bae1b75b1a4c1c973a941b110
单强 <shanqiang@sunline.cn>
2019-06-14 11:38:41 +0800

    JVM GC 强引用Java8中OOM失败

--- src/main/java/jvm/gc/is_garbage/ref/StrongRef.java
+++ src/main/java/jvm/gc/is_garbage/ref/StrongRef.java
@@ -5,7 +5,17 @@ package jvm.gc.is_garbage.ref;
  * 也就是说，宁愿出现内存溢出，也不会回收这些对象。
  */
 public class StrongRef {
+
+    private static int _1MB = 1024*1024;
+    private byte[] bigSize = new byte[2*_1MB];
+
     public static void main(String[] args) {
-        Object obj = new Object();
+        StrongRef[] StrongRefArr = new StrongRef[1000];
+    }
+
+    public static void fun1() {
+        Object object = new Object();
+        StrongRef[] StrongRefArr = new StrongRef[1000];
+        System.gc();
     }
 }
