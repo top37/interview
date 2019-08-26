2d4385b2874ff7b78dfd945519653bce85423898
单强 <shanqiang@sunline.cn>
2019-08-26 17:41:47 +0800

    String 去重

--- src/main/java/base/algorithm/RmRepeatCharWithRegex.java
+++ src/main/java/base/algorithm/RmRepeatCharWithRegex.java
@@ -8,12 +8,13 @@ public class RmRepeatCharWithRegex {
     }
 
     public String removeRepeat(String str) {
-        //炫技 start 无卵用
+        //非炫技 start 有用 保证顺序
         StringBuffer sb = new StringBuffer(str);
         String rs = sb.reverse().toString().replaceAll("(.)(?=.*\\1)", "");
         StringBuffer out = new StringBuffer(rs);
-        //炫技 end 无卵用
-        return str.replaceAll("(.)(?=.*\\1)", "");
+        //非炫技 end 有用 保证顺序
+//        return str.replaceAll("(.)(?=.*\\1)", "");
+        return out.reverse().toString();
     }
 
 }
